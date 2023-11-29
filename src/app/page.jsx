b5d5ab60abfe403a2e9fdc172a853310c79b69e9
vishal <ivishalgautamm@gmail.com>
2023-11-29 11:37:35 +0530

    added google analytics tags

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 import CategoriesHome from "@/components/CategoriesHome";
 import Hero from "@/components/Hero";
+import Script from "next/script";
 import React from "react";
 
 export const metadata = {
@@ -8,6 +9,18 @@ export const metadata = {
 export default function Page() {
   return (
     <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-5W81C4E1WN"
+      />
+      <Script>
+        {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-5W81C4E1WN');`}
+      </Script>
+
       <Hero />
       <CategoriesHome />
     </>
