ea193517b8167b8f317a2be9edd5fbd9be8c5293
vishal <ivishalgautamm@gmail.com>
2023-12-05 17:28:13 +0530

    added new meta tags

---
+++ src/app/head.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="kWcwy0Kag9MmpnCSMcrOL7VuQT5ZKjuBbZ6218QCpZw"
+      />
+    </>
+  );
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -16,19 +16,32 @@ export default function Page() {
       />
       <Script>
         {`window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-  gtag('config', 'G-5W81C4E1WN');`}
+          gtag('config', 'G-5W81C4E1WN');`}
       </Script>
-      <Head>
-        <meta
-          name="google-site-verification"
-          content="kWcwy0Kag9MmpnCSMcrOL7VuQT5ZKjuBbZ6218QCpZw"
-        />
-      </Head>
+      {/* <!-- Google Tag Manager --> */}
+      <script>
+        {`function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WN9SB6DD');`}
+      </script>
+      {/* <!-- End Google Tag Manager --> */}
       <Hero />
       <CategoriesHome />
+      {/* Google Tag Manager (noscript)  */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-WN9SB6DD"
+          height="0"
+          width="0"
+          style="display:none;visibility:hidden"
+        ></iframe>
+      </noscript>
+      {/* End Google Tag Manager (noscript)  */}
     </>
   );
 }
