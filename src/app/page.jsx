c88456e4878f126e02fd683d9f27af8a2c840cb9
vishal <ivishalgautamm@gmail.com>
2023-12-05 17:30:15 +0530

    added new meta tags

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -21,14 +21,15 @@ export default function Page() {
 
           gtag('config', 'G-5W81C4E1WN');`}
       </Script>
+
       {/* <!-- Google Tag Manager --> */}
-      <script>
+      <Script>
         {`function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-WN9SB6DD');`}
-      </script>
+      </Script>
       {/* <!-- End Google Tag Manager --> */}
       <Hero />
       <CategoriesHome />
