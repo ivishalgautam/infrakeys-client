fd0570cc10522400231d06368385b40036990c65
vishal <ivishalgautamm@gmail.com>
2023-12-09 15:37:41 +0530

    added blogs page

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -9,6 +9,14 @@ import toast from "react-hot-toast";
 import { MdSkipNext, MdSkipPrevious } from "react-icons/md";
 import "../../styles/pagination.css";
 
+export const metadata = {
+  title: "Blog Infrakeys | Steel Manufacturers Faridabad,India",
+  description:
+    "Know latest blog and articles for Infrakeys Steel Fabricators,Industrial Steel,Steel supplier,scaffolding manufacturers,TMT suppliers,PEB manufacturers,Roofing,steel raw material.",
+  keywords:
+    "steel manufacturers in faridabad,steel fabricators nera me,industrial steel in faridabad,Steel supplier in India,scaffolding manufacturer in faridabad,TMT suppliers in Faridabad,PEB manufacturers in India, PEB manufacturers in faridabad,Steel supplier in faridabad,top steel manufacturers in faridabad,india,Steel Fabricators faridabad,steel manufacturers ner me,steel raw material, quality building products,roofing materials,",
+};
+
 export default function Blogs() {
   const [blogs, setBlogs] = useState([]);
 
