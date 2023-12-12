798c79247ff6ece392bcbb24ecdbdb7755c804a0
vishal <ivishalgautamm@gmail.com>
2023-12-12 13:06:49 +0530

    added sitemap

--- src/app/sitemap.jsx
+++ src/app/sitemap.jsx
@@ -1,34 +1,37 @@
 import { publicRequest } from "@/libs/requestMethods";
 
 export default async function Sitemap() {
-  const baseUrl = "https://infrakeys.com";
+  const baseUrl = "https://www.infrakeys.com";
 
   // sub-categories urls
   const subResp = await publicRequest.get("/sub-categories");
-  const subUrls = subResp?.data?.map((sub) => {
-    return {
-      url: `${baseUrl}/sub-categories/${sub.slug}`,
-      lastModified: new Date(),
-    };
-  });
+  const subUrls =
+    subResp?.data?.map((sub) => {
+      return {
+        url: `${baseUrl}/sub-categories/${sub.slug}`,
+        lastModified: new Date(),
+      };
+    }) ?? [];
 
   //blogs urls
   const blogResp = await publicRequest.get("/blogs");
-  const blogUrls = blogResp?.data?.map((blog) => {
-    return {
-      url: `${baseUrl}/blogs/${blog.slug}`,
-      lastModified: new Date(),
-    };
-  });
+  const blogUrls =
+    blogResp?.data?.map((blog) => {
+      return {
+        url: `${baseUrl}/blogs/${blog.slug}`,
+        lastModified: new Date(),
+      };
+    }) ?? [];
 
   //product urls
   const productResp = await publicRequest.get("/products");
-  const productUrls = productResp?.data?.map((product) => {
-    return {
-      url: `${baseUrl}/products/${product.slug}`,
-      lastModified: new Date(),
-    };
-  });
+  const productUrls =
+    productResp?.data?.map((product) => {
+      return {
+        url: `${baseUrl}/products/${product.slug}`,
+        lastModified: new Date(),
+      };
+    }) ?? [];
 
   return [
     {
