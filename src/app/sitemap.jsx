abfff0cb9257c71509f7e68c9195049cc7088152
vishal <ivishalgautamm@gmail.com>
2023-12-12 12:59:06 +0530

    added sitemap

--- src/app/sitemap.jsx
+++ src/app/sitemap.jsx
@@ -5,24 +5,30 @@ export default async function Sitemap() {
 
   // sub-categories urls
   const subResp = await publicRequest.get("/sub-categories");
-  const subUrls = subResp?.data?.map((sub) => ({
-    url: `${baseUrl}/sub-categories/${sub.slug}`,
-    lastModified: new Date(),
-  }));
+  const subUrls = subResp?.data?.map((sub) => {
+    return {
+      url: `${baseUrl}/sub-categories/${sub.slug}`,
+      lastModified: new Date(),
+    };
+  });
 
   //blogs urls
   const blogResp = await publicRequest.get("/blogs");
-  const blogUrls = blogResp?.data?.map((blog) => ({
-    url: `${baseUrl}/blogs/${blog.slug}`,
-    lastModified: new Date(),
-  }));
+  const blogUrls = blogResp?.data?.map((blog) => {
+    return {
+      url: `${baseUrl}/blogs/${blog.slug}`,
+      lastModified: new Date(),
+    };
+  });
 
   //product urls
   const productResp = await publicRequest.get("/products");
-  const productUrls = productResp?.data?.map((product) => ({
-    url: `${baseUrl}/products/${product.slug}`,
-    lastModified: new Date(),
-  }));
+  const productUrls = productResp?.data?.map((product) => {
+    return {
+      url: `${baseUrl}/products/${product.slug}`,
+      lastModified: new Date(),
+    };
+  });
 
   return [
     {
