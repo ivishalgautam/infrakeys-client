ce53441087a70572607a84ab15fc988ea1bd9422
vishal <ivishalgautamm@gmail.com>
2023-12-12 10:37:04 +0530

    added sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -7,21 +7,21 @@ export default async function sitemap() {
   const subResp = await publicRequest.get("/sub-categories");
   const subUrls = subResp?.data?.map((sub) => ({
     url: `${baseUrl}/sub-categories/${sub.slug}`,
-    lastModified: new Date(sub.updated_at),
+    lastModified: new Date(),
   }));
 
   //blogs urls
   const blogResp = await publicRequest.get("/blogs");
   const blogUrls = blogResp?.data?.map((blog) => ({
     url: `${baseUrl}/blogs/${blog.slug}`,
-    lastModified: new Date(sub.updated_at),
+    lastModified: new Date(),
   }));
 
   //product urls
   const productResp = await publicRequest.get("/products");
   const productUrls = productResp?.data?.map((product) => ({
     url: `${baseUrl}/products/${product.slug}`,
-    lastModified: new Date(sub.updated_at),
+    lastModified: new Date(),
   }));
 
   return [
