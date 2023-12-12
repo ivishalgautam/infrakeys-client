30f157d240ed7a41a10fa18170e907434f71a9bb
vishal <ivishalgautamm@gmail.com>
2023-12-12 12:36:01 +0530

    added sitemap

--- src/app/sub-categories/[slug]/page.jsx
+++ src/app/sub-categories/[slug]/page.jsx
@@ -1,96 +1,29 @@
+"use client";
 import CenterHeading from "@/components/CenterHeading";
 import Viewmore from "@/components/Viewmore";
 import { publicRequest } from "@/libs/requestMethods";
 import Image from "next/image";
 import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
-export async function generateStaticParams() {
-  const categories = await publicRequest.get(`/sub-categories`);
-  // console.log(categories);
-  if (!categories) return [];
-  return categories?.data?.map((category) => ({
-    slug: category.slug.toString(),
-  }));
-}
-
-export async function getServerSideProps({ params: { slug } }) {
-  try {
-    await new Promise(async (resolve) => {
-      const data = await publicRequest.get(`/sub-categories/slug/${slug}`);
-      resolve();
-      return data;
-    })
+export default function Page({ params: { slug } }) {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    publicRequest
+      .get(`/sub-categories/slug/${slug}`)
+      .then(({ data }) => {
+        return data[0];
+      })
       .then(async (data) => {
         const resp = await publicRequest.get(`/products`);
-        const products = await resp?.data?.filter(
-          (item) => item.sub_category_id === data?.id
+        setProducts(
+          resp.data.filter((item) => item.sub_category_id === data.id)
         );
-        return {
-          props: {
-            slug: slug,
-            products,
-          },
-        };
       })
       .catch((error) => {
-        return {
-          props: {
-            slug: slug,
-            products: [],
-          },
-        };
+        console.log(error);
       });
-
-    // console.log({ data, resp, products });
-  } catch (error) {
-    console.log(error);
-    return {
-      props: {
-        slug: slug,
-        products: [],
-      },
-    };
-  }
-}
-
-export async function generateMetadata({ params: { slug } }) {
-  try {
-    const { data } = await publicRequest.get(`/sub-categories/slug/${slug}`);
-
-    if (!data) {
-      return {
-        title: "Not Found!",
-        description: "The page you you looking for does not exist!",
-        keywords: "Not found",
-      };
-    }
-    return {
-      title: data?.title,
-      description: data?.about,
-      keywords: data?.keywords,
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      title: "Not Found!",
-      description: "The page you you looking for does not exist!",
-      keywords: "Not found",
-    };
-  }
-}
-
-export default function Page({ params: { slug, products } }) {
-  // const [products, setProducts] = useState([]);
-  // useEffect(() => {
-  //   publicRequest
-  //     .get(`/sub-categories/slug/${slug}`)
-  //     .then(({ data }) => {
-  //       return data[0];
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // }, [slug]);
+  }, [slug]);
   return (
     <>
       <section className="commonSection">
@@ -100,7 +33,7 @@ export default function Page({ params: { slug, products } }) {
             {products?.map((product, key) => {
               return (
                 <div className="col-lg-3 col-md-4 col-sm-6 mb-4">
-                  <div className="productCard" key={product?.id}>
+                  <div className="productCard" key={product.id}>
                     <div className="productImg">
                       <Image
                         src={`https://infrakeysapp.in${product?.images[0]}`}
@@ -114,11 +47,16 @@ export default function Page({ params: { slug, products } }) {
                         <h3>{product?.title}</h3>
                       </Link>
                       <p>
-                        {product?.about?.length > 150
-                          ? product?.about?.substring(0, 150) + "..."
-                          : product?.about}
+                        {product.about.length > 150
+                          ? product.about.substring(0, 150) + "..."
+                          : product.about}
                       </p>
-                      <Viewmore viewLink={`/products/${product?.slug}`} />
+                      <Viewmore
+                        viewLink={`/products/${product?.title
+                          .toLowerCase()
+                          .split(" ")
+                          .join("-")}/${product?.id}`}
+                      />
                     </div>
                   </div>
                 </div>
